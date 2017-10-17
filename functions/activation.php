7422229f971d3539fb710b321a7758f56291dc31
Ville Aro <ville_aro@icloud.com>
2017-10-17 13:03:49 +0200

    first commit

---
+++ functions/activation.php
@@ -0,0 +1,49 @@
+<?php
+
+function va_employees_setup_post_type() {
+
+	$args = array(
+	'labels' => array(
+		'name' => __('Employees'),
+		'singular_name' => __('Employee'),
+		'all_items' => __('All Employees'),
+		'add_new_item' => __('Add New Employee'),
+		'edit_item' => __('Edit Employee'),
+		'view_item' => __('View Employee')
+	),
+	'public' => false,
+	'publicly_queryable' => false,
+	'has_archive' => false,
+	'rewrite' => array('slug' => 'employees'),
+	'show_ui' => true,
+	'show_in_menu' => true,
+	'show_in_nav_menus' => true,
+	'show_in_admin_bar' => true,
+	'capability_type' => 'page',
+	'supports' => array('title', 'editor', 'thumbnail'),
+	'exclude_from_search' => true,
+	'menu_position' => 20,
+	'has_archive' => false,
+	'menu_icon' => 'dashicons-format-quote'
+	);
+	
+	// https://codex.wordpress.org/Function_Reference/register_post_type
+	register_post_type('va-employees', $args);
+
+}
+add_action( 'init', 'va_employees_setup_post_type' );
+
+
+
+
+
+function va_employees_install() {
+
+    // trigger our function that registers the custom post type
+    va_employees_setup_post_type();
+ 
+    // clear the permalinks after the post type has been registered
+    flush_rewrite_rules();
+
+}
+register_activation_hook( __FILE__, 'va_employees_install' );
