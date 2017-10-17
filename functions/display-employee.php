9092341b8a826cb71b81cf409cfb7df7bac71a8f
Ville Aro <ville_aro@icloud.com>
2017-10-17 18:26:51 +0200

    added style attribute to shortcode

--- functions/display-employee.php
+++ functions/display-employee.php
@@ -1,6 +1,6 @@
 <?php
 
-function va_employees_display_employee() {
+function va_employees_display_employee( $style ) {
 
     $post_id = get_the_ID();
     $employee_job = get_post_meta( $post_id, 'employee_job', true );
@@ -9,33 +9,46 @@ function va_employees_display_employee() {
     $employee_website_url = esc_url( get_post_meta( $post_id, 'employee_website_url', true ) );
     $image = get_the_post_thumbnail_url( $post_id, 'medium' );
 
-    $output = '<div class="col"><div class="employee">';
+    $output = '<div class="col">';
 
-        if ( $image ) {
-            $output .= '<img src="' . $image . '" />';
+        if ( $style == 'boxed' ) {
+            $output .= '<div class="box">';
         }
 
-        $output .= '<div class="info">';
-            $output .= '<h4>' . get_the_title() . '</h4>';
+        $output .= '<div class="employee">';
 
-            if ( $employee_job ) {
-                $output .= '<div class="job">' . $employee_job . '</div>';
+            if ( $image ) {
+                $output .= '<img src="' . $image . '" />';
             }
 
-            if ( $employee_email ) {
-                $output .= '<div class="email">' . $employee_email . '</div>';
-            }
+            $output .= '<div class="info">';
+                $output .= '<h4>' . get_the_title() . '</h4>';
 
-            if ( $employee_phone ) {
-                $output .= '<div class="phone">' . $employee_phone . '</div>';
-            }
+                if ( $employee_job ) {
+                    $output .= '<div class="job">' . $employee_job . '</div>';
+                }
 
-            if ( $employee_website_url ) {
-                $output .= '<a href="' . $employee_website_url . '" target="_blank">' . $employee_website_url . '</a>';
-            }
-        $output .= '</div>';
+                if ( $employee_email ) {
+                    $output .= '<div class="email">' . $employee_email . '</div>';
+                }
 
-    $output .= '</div></div>';
+                if ( $employee_phone ) {
+                    $output .= '<div class="phone">' . $employee_phone . '</div>';
+                }
+
+                if ( $employee_website_url ) {
+                    $output .= '<a href="' . $employee_website_url . '" target="_blank">' . $employee_website_url . '</a>';
+                }
+            $output .= '</div>'; // Close .info
+
+        $output .= '</div>'; // Close .employee
+
+        if ( $style == 'boxed' ) {
+            $output .= '</div>'; // Close .box
+        }
+    
+    $output .= '</div>'; // Close .col
+
     echo $output;
 
 }
--- functions/shortcode.php
+++ functions/shortcode.php
@@ -6,23 +6,26 @@ function va_employees_shortcode( $atts ) {
  
     // define attributes and their defaults
     extract( shortcode_atts( array (
+        'style' => '',
+        'columns' => 1,
         'order' => 'desc',
         'orderby' => 'date',
         'posts' => -1,
-        'columns' => 3,
         'ids' => array(),
     ), $atts ) );
 
-    if ( $ids ) {
-        $id_array = explode(',', $ids);
-    } else {
-        $id_array = '';
-    }
-
+    // Set columns_mobile variable
     if ( $columns < 3 ) {
         $columns_mobile = 1;
     } else {
         $columns_mobile = 2;
+    }
+
+    // Exlpode ids
+    if ( $ids ) {
+        $id_array = explode(',', $ids);
+    } else {
+        $id_array = '';
     }
 
     // define query parameters based on attributes
@@ -31,26 +34,27 @@ function va_employees_shortcode( $atts ) {
         'order' => $order,
         'orderby' => $orderby,
         'posts_per_page' => $posts,
-        'columns' => $columns,
         'post__in' => $id_array,
     );
     $query = new WP_Query( $options );
 
+    // Loop
+    if ( $query->have_posts() ) {
+        echo '<div class="nested grid-' . $columns . '_md-' . $columns_mobile . '_sm-1">';
 
-    if ( $query->have_posts() ) : ?>
-        <div class="nested grid-<?php echo $columns ?>_md-<?php echo $columns_mobile ?>_sm-1">
-
-            <?php while ( $query->have_posts() ) :
+            while ( $query->have_posts() ) {
+                
                 $query->the_post();
 
-                va_employees_display_employee();
+                // file: functions/display-employee.php
+                va_employees_display_employee( $style );
 
-            endwhile;
+            }
 
-            wp_reset_postdata(); ?>
+            wp_reset_postdata(); 
 
-        </div>
-    <?php endif;
+        echo '</div>';
+    };
 
     $output = ob_get_clean();
     return $output;
